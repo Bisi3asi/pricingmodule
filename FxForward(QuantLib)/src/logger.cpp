3f68d88d3bd97cddfea4cf65f055c72bce6d969e
Bisi3asi <kueeng8008@naver.com>
2025-05-22 19:00:30 +0900

    feat: logger 생성 시 오류 처리 및 파일 생성경로 표시 수정

--- FxForward(QuantLib)/src/logger.cpp
+++ FxForward(QuantLib)/src/logger.cpp
@@ -1,17 +1,25 @@
-#include "logger.h"
+ï»¿#include "logger.h"
 
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/basic_file_sink.h>
 #include <filesystem>
+#include <iostream>
 
-/* ÆÄÀÏ ·Î°Å¸¦ »ı¼ºÇÑ´Ù. */
+/* íŒŒì¼ ë¡œê±°ë¥¼ ìƒì„±í•œë‹¤. */
 void initLogger(const std::string& filename) {
-    std::filesystem::create_directories("../logs");
+    try {
+        std::string full_path = std::filesystem::weakly_canonical("../logs/" + filename).string();
+        std::cout << "Log file will be created at: " << full_path << std::endl;
 
-    if (!spdlog::get("logger")) {
-        auto file_logger = spdlog::basic_logger_mt("logger", "../logs/" + filename, true); // overwrite : true
-        spdlog::set_default_logger(file_logger);
-        spdlog::set_level(spdlog::level::info);
-        spdlog::set_pattern("[%l][%Y-%m-%d %H:%M:%S] %v");
+        if (!spdlog::get("logger")) {
+            auto file_logger = spdlog::basic_logger_mt("logger", full_path, true);
+            spdlog::set_default_logger(file_logger);
+            spdlog::set_level(spdlog::level::info);
+            spdlog::set_pattern("[%l][%Y-%m-%d %H:%M:%S] %v");
+        }
     }
+    catch (const spdlog::spdlog_ex& ex) {
+        std::cerr << "Failed to create logger: " << ex.what() << std::endl;
+    }
+
 }
