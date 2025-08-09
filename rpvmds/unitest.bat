cbd809a7fd569b9936bf74aa8549a8c9303e9198
Bisi3asi <kueeng8008@naver.com>
2025-08-09 10:27:00 +0900

    chore: windows 기준 rpvmds 설정 변경

--- rpvmds/unitest.bat
+++ rpvmds/unitest.bat
@@ -3,7 +3,7 @@
 echo RPVMds Evaluator Start
 if "-console"=="%1" (
     echo Enter the data you want to analyze
-    java -jar "%RPVMDS_HOME%\lib\rpvmds-evaluater-0.0.1.jar"
+    java -jar "%RPVMDS_HOME%\lib\rpvmds-evaluater-1.0.1.jar"
 ) else (
     echo UnitTest UI Open 
     java %RPVMS_J_OPTS% -classpath "%RPVMDS_HOME%\lib\rpvmds.jar" com.icis.RPVM.ds.test.RPVMSimul
