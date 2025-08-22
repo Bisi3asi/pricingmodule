b6c8b7752a6760828cbe9d015eb1411f53fca825
Bisi3asi <kueeng8008@naver.com>
2025-08-22 17:55:22 +0900

    feat: Net 상품모듈

---
+++ Net/src/net.cpp
@@ -0,0 +1,43 @@
+﻿#include "net.h"
+#include "logger.hpp"
+#include "common.hpp"
+
+// namespace
+using namespace QuantLib;
+using namespace std;
+using namespace logger;
+
+extern "C" double EXPORT pricingNET(
+      const int evaluationDate                // INPUT 1. 평가일 (serial number)
+    , const double notional                   // INPUT 2. 채권 원금
+	, const int logYn 					      // INPUT 3. 로깅 여부 (0: No, 1: Yes)
+) {
+    /* 로거 종료 */
+    FINALLY(
+        info("==============[Net: pricingNET Logging Ended!]==============");
+        closeLogger();
+    );
+
+    try {
+        /* 로거 초기화 */
+        disableConsoleLogging(); // 로깅여부 N일시 콘촐 입출력 비활성화
+        if (logYn == 1) {
+            initLogger("net.log"); // 생성 파일명 지정
+        }
+		info("==============[Net: pricingNET Logging Started!]==============");
+        return notional;
+    }
+    catch (...) {
+        try {
+            std::rethrow_exception(std::current_exception());
+        }
+        catch (const std::exception& e) {
+            error("Exception occurred: {}", string(e.what()));
+            return -1;
+        }
+        catch (...) {
+            error("[pricingNet]: Unknown exception occurred.");
+            return -1;
+        }
+    }
+}
---
+++ Net/src/net.h
@@ -0,0 +1,30 @@
+﻿#ifndef BOND_H
+#define BOND_H
+
+// function 외부 인터페이스 export 정의
+#ifdef _WIN32
+#ifdef BUILD_LIBRARY
+#define EXPORT __declspec(dllexport) __stdcall
+#else
+#define EXPORT __declspec(dllimport) __stdcall
+#endif
+#elif defined(__linux__) || defined(__unix__)
+#define EXPORT
+#endif
+
+#pragma once
+
+/* include */
+#include <iostream>
+#include <iomanip>
+
+/* include(QuantLib) */
+
+/* dll export method(extern "C", EXPORT 명시 필요) */
+extern "C" double EXPORT pricingNET(
+    const int evaluationDate                // INPUT 1. 평가일 (serial number)
+  , const double notional                   // INPUT 2. 채권 원금
+  , const int logYn 					    // INPUT 3. 로깅 여부 (0: No, 1: Yes)
+);
+
+#endif
