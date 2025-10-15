0fa2c14ebe792a86ad6dbc84c393dff38863cbb9
Bisi3asi <kueeng8008@naver.com>
2025-10-15 18:19:49 +0900

    feat, fix: logger 파일명 타임스탬프 추가, 기타 로깅용 편의성 매크로 추가

--- CommonUtils/src/logger.cpp
+++ CommonUtils/src/logger.cpp
@@ -1,5 +1,8 @@
 ﻿#include "logger.hpp"
+
 #include <spdlog/sinks/null_sink.h>
+#include <chrono>
+#include <ctime>
 
 namespace logger {
 
@@ -11,15 +14,38 @@ namespace logger {
     }
 
     /* 로거 생성 */
-    void initLogger(const std::string& filename, const char* funcName = nullptr) {
+    void initLogger(const std::string& filename, const char* funcName) {
         try {
             std::filesystem::path log_dir = "./logs"; // 로깅 디렉토리 지정
             if (!std::filesystem::exists(log_dir)) {
                 std::filesystem::create_directories(log_dir); // 해당 디렉토리 없을 시 생성
             }
+        
+            // 로깅 파일에 타임스탬프 추가
+            auto now = std::chrono::system_clock::now();
+            auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000;
+            std::time_t tnow = std::chrono::system_clock::to_time_t(now);
+            std::tm local_tm;
 
-            std::string full_path = std::filesystem::weakly_canonical(log_dir / filename).string();
+            #if defined(_MSC_VER) // MSVC
+                localtime_s(&local_tm, &tnow);
+            #else // Linux/Unix
+                localtime_r(&tnow, &local_tm);
+            #endif
+
+            std::ostringstream tsoss;
+            // 타임스탬프 형식 ex) 20251015.123045.123
+            tsoss << std::put_time(&local_tm, "%Y%m%d") << '.'
+                << std::put_time(&local_tm, "%H%M%S") << '.'
+                << std::setw(3) << std::setfill('0') << ms.count();
+
+            std::filesystem::path in_path = filename;
+            std::string base = in_path.filename().string();
+            std::string out_name = base + "_" + tsoss.str() + ".log";
+
+            std::string full_path = std::filesystem::weakly_canonical(log_dir / out_name).string();
             std::cout << "Log file will be created at: " << full_path << std::endl;
+            std::cout << std::endl;
 
             if (!spdlog::get("logger")) {
                 auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(full_path, /*truncate=*/true);
@@ -35,6 +61,7 @@ namespace logger {
 
             if (funcName) {
                 info("[{}] - Logging Start.", funcName);
+                info("");
             }
         }
         catch (const spdlog::spdlog_ex& ex) {
@@ -43,11 +70,15 @@ namespace logger {
     }
 
     /* 로거 종료 및 해제 */
-    void closeLogger(const char* funcName = nullptr) {
+    void closeLogger(const double result) {
         try {
-            if (funcName) {
-                info("[{}] - Logging End.", funcName);
+            if (result >= 0.0) {
+                info("Pricing Process Success.");
             }
+            else {
+                error("Pricing Process Fail with Errors: {}.", result);
+            }
+            info("Logging End.");
             spdlog::shutdown();  // 모든 로거 정리 및 해제
         }
         catch (const spdlog::spdlog_ex& ex) {
--- CommonUtils/src/logger.hpp
+++ CommonUtils/src/logger.hpp
@@ -1,5 +1,4 @@
 ﻿#pragma once
-
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/sinks/basic_file_sink.h>
@@ -9,12 +8,34 @@
 #include <iomanip>
 #include <iostream>
 #include <filesystem>
+#include <sstream>
+#include <type_traits> 
+
+#define LOG_START(fileName) logger::initLogger(fileName, __func__)
+#define LOG_END(result) logger::closeLogger(result)
+#define LOG_VAR(x) logger::Loggable<decltype(x)>::log(#x, (x))
+#define LOG_ARRAY(ptr, size, ...) logger::logArrayLine(#ptr "[]", (ptr), (size), ##__VA_ARGS__)
+#define LOG_ENTER_PRICING() logger::enterPricing()
+#define LOG_KNOWN_EXCEPTION_ERROR(msg) logger::logKnownExceptionError(msg)
+#define LOG_UNKNOWN_EXCEPTION_ERROR() logger::logUnknownExceptionError()
+
+// 배열 로깅을 위한 헬퍼 구조체
+struct ArrayInt {
+    const int* data;
+    size_t size;
+};
+
+struct ArrayDouble {
+    const double* data;
+    size_t size;
+    int precision = -1;
+};
 
 namespace logger {
 
     void disableConsoleLogging();
     void initLogger(const std::string& filename, const char* funcName = nullptr);
-    void closeLogger(const char* funcName = nullptr);
+    void closeLogger(const double result);
 
     // 기존 포맷팅 지원 템플릿
     template <typename... Args>
@@ -31,7 +52,6 @@ namespace logger {
         }
     }
 
-    /**/
     // 오버로드: 단순 문자열(const char*) 로그용
     inline void info(const char* msg) {
         if (auto logger = spdlog::default_logger()) {
@@ -58,28 +78,69 @@ namespace logger {
         }
     }
 
+    inline void enterPricing() {
+        info("Pricing Process Start.");
+        info("");
+    }
+
+    inline void logKnownExceptionError(const std::string& msg) {
+        error("! Exception Occured !");
+        error("---------------------------------------------");
+        error("{}", msg);
+        error("---------------------------------------------");
+        info("");
+    }
+
+    inline void logUnknownExceptionError() {
+        error("! Exception Occured !");
+        error("---------------------------------------------");
+        error("Unknown Exception, please check the input parameters and environment.");
+        error("---------------------------------------------");
+        info("");
+    }
+
     // 배열/벡터 출력 헬퍼
     template <typename T>
-    void logArrayLine(const std::string& label, const T* data, size_t size, int precision = -1) {
+    void logArrayLine(const std::string& label, const T* data, size_t size, int precision = 10) {
         std::ostringstream oss;
         if (precision >= 0)
             oss << std::fixed << std::setprecision(precision);
-        oss << label << ": ";
+        oss << label << " = {";
         for (size_t i = 0; i < size; ++i) {
             oss << data[i];
             if (i != size - 1)
                 oss << ", ";
         }
-        info(oss.str());
+        oss << "};";
+        info("{}", oss.str());
     }
 
     template <typename T>
-    void logArrayLine(const std::string& label, const std::vector<T>& data, int precision = -1) {
+    void logArrayLine(const std::string& label, const std::vector<T>& data, int precision = 10) {
         logArrayLine(label, data.data(), data.size(), precision);
     }
-}
 
-// 로깅용 매크로 정의
-#define LOG_START(fileName) logger::initLogger(fileName, __func__)
-#define LOG_END() logger::closeLogger(__func__)
-#define LOG_VAR(x) logger::info("{} = {}", #x, x) // 변수명 = 값
+    // 타입 이름을 문자열로 변환하는 헬퍼 
+    template <typename T>
+    constexpr const char* type_name() {
+        using U = std::remove_cv_t<std::remove_reference_t<T>>;
+        if constexpr (std::is_same_v<U, int>) return "int";
+        if constexpr (std::is_same_v<U, double>) return "double";
+        if constexpr (std::is_same_v<U, const char*>) return "const char*";
+        if constexpr (std::is_same_v<U, std::string>) return "std::string";
+        if constexpr (std::is_same_v<U, ArrayInt>) return "int[]";
+        if constexpr (std::is_same_v<U, ArrayDouble>) return "double[]";
+        return "unknown_type";
+    }
+
+    template<typename T>
+    struct Loggable {
+        static void log(const char* name, const T& value) {
+            // 예: double evaluationDate = 45107;
+            info("{} {} = {};", type_name<T>(), name, value);
+        }
+    };
+
+} // namespace logger
+
+
